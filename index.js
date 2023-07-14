addd046d3eb79f782b57475db09022ee69637cea
Prasad Arun Dhobale <dhobaleprasad3@gmail.com>
2023-07-15 02:07:17 +0530

    updated.

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-import express from 'express';
-import nodemailer from 'nodemailer';
-
+const express = require('express');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
-app.get('/', (req, res) => {
-
-
-
+app.get('/', async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'services.propad@gmail.com',
       pass: 'lmiqsegofxgsjbnl'
     }
-  });  
+  });
 
   const email = req.query.owneremail;
   const name = req.query.name;
@@ -23,28 +19,23 @@ app.get('/', (req, res) => {
   const umsg = req.query.body;
   const url = req.query.url;
 
-
   const mailOptions = {
     from: 'services.propad@gmail.com',
     to: email,
-    subject: `${name } Contacted from Portfolio`,
+    subject: `${name} Contacted from Portfolio`,
     text: `Contact Form Details \n\nName : ${name}\nEmail : ${uemail}\nSubject : ${usubject}\nBody : ${umsg}`
   };
 
   console.log(`OwnerEmail : ${email}\nName: ${name}\nemail: ${uemail}\nsubject : ${usubject}\numsg : ${umsg}\nUrl : ${url}`);
 
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-      
-    }
-  });
-
-  res.redirect(url);
-
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
+    res.redirect(url);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error sending email');
+  }
 });
 
 const PORT = process.env.PORT || 8000;
